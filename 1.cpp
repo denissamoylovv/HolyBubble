9a34b6fbe8a6141fece3783916eb520b37824845
denissamoylovv <denissamoylovv+git@gmail.com>
2016-09-09 08:59:12 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,13 +3,16 @@
 int main(){
 	int i=0,a,b;
 	double av=0,ave=0;
+	FILE *f;
 	
-	while(scanf("%d",&a)==1){
+	f=fopen("1",r);
+	while(fscanf(f,"%d",&a)==1){
 		av+=a;
 		ave+=(a*a);
 		i++;
 	}
 	printf("g %g\n",(ave/i-av*av/i/i));
+	fclose(f);
 	
 	return 0;
 }
